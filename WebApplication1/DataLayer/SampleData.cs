075c367a818e74c1e1c8e3b2085aaa41c82e4738
MorozovAP <andre15@inbox.ru>
2019-06-20 14:57:56 +0400

    Data layer

---
+++ WebApplication1/DataLayer/SampleData.cs
@@ -0,0 +1,41 @@
+﻿using System.Linq;
+using DataLayer.Entities;
+using Microsoft.EntityFrameworkCore.Internal;
+
+namespace DataLayer
+{
+    public static class SampleData
+    {
+        public static void InitData(EFDBContext context)
+        {
+            if (!EnumerableExtensions.Any(context.Directories))
+            {
+                context.Directories.Add(new Directory() {Title = "First directory", Html = "<b>Directory content 1</b>"});
+                context.Directories.Add(new Directory() {Title = "Second directory", Html = "<b>Directory content 2</b>"});
+                context.Directories.Add(new Directory() {Title = "Third directory", Html = "<b>Directory content 3</b>"});
+                context.SaveChanges();
+
+                context.Materials.Add(new Material()
+                {
+                    Title = "First material",
+                    Html = "<i>Material content 1</i>",
+                    DirectoryId = context.Directories.First().Id
+                });
+                context.Materials.Add(new Material()
+                {
+                    Title = "Second material",
+                    Html = "<i>Material content 2</i>",
+                    DirectoryId = context.Directories.First().Id
+                });
+                context.Materials.Add(new Material()
+                {
+                    Title = "Third material",
+                    Html = "<i>Material content 3</i>",
+                    DirectoryId = context.Directories.Last().Id
+                });
+
+                context.SaveChanges();
+            }
+        }
+    }
+}
--- WebApplication1/WebApplication1/Startup.cs
+++ WebApplication1/WebApplication1/Startup.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DataLayer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -24,6 +26,10 @@ namespace WebApplication1
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            var connection = Configuration.GetConnectionString("DefaultConnection");
+            services.AddDbContext<EFDBContext>(options =>
+                options.UseSqlServer(connection, b => b.MigrationsAssembly("DataLayer")));
+
             services.Configure<CookiePolicyOptions>(options =>
             {
                 // This lambda determines whether user consent for non-essential cookies is needed for a given request.
